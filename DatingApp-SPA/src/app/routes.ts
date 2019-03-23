2fbf1c7d6c855152e0f288caf87eb6274aade15a
israel moreira <israelluze@gmail.com>
2019-03-23 18:39:10 -0300

    Detalhamento dos usuarios

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -2,9 +2,10 @@ import { AuthGuard } from './_guards/auth.guard';
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -14,6 +15,7 @@ export const appRoutes: Routes = [
     canActivate: [AuthGuard],
     children: [
       { path: 'members', component: MemberListComponent },
+      { path: 'members/:id', component: MemberDetailComponent },
       { path: 'messages', component: MessagesComponent },
       { path: 'lists', component: ListsComponent }
     ]
