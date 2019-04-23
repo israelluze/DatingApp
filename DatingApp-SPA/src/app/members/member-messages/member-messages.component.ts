157e9e74d47a561a27b156adcf7535db1c809b04
Israel Moreira <Israel.Moreira@cetil.com.br>
2019-04-23 20:06:18 -0300

    commit

--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -12,6 +12,7 @@ import { UserService } from 'src/app/_services/user.service';
 export class MemberMessagesComponent implements OnInit {
 @Input() recipientId: number;
 messages: Message[];
+newMessage: any = {};
 
   constructor(private userService: UserService, private authService: AuthService,
               private alertify: AlertifyService) { }
@@ -27,6 +28,17 @@ messages: Message[];
       }, error => {
         this.alertify.error(error);
       });
+  }
+
+  sendMessage() {
+    this.newMessage.recipientId = this.recipientId;
+    this.userService.sendMessage(this.authService.decodedToken.nameid, this.newMessage)
+    .subscribe((message: Message) => {
+      this.messages.unshift(message);
+      this.newMessage.content = '';
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 
 }
